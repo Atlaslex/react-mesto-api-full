821a08623694e1c4fab1ed434346588cbc716243
Atlaslex <atlaslex@yandex.ru>
2022-10-28 16:11:24 +1200

    0001_08

--- backend/errors/NotUniqueEmail.js
+++
@@ -1,7 +0,0 @@
-module.exports = class NotUniqueEmailError extends Error {
-  constructor(message) {
-    super(message);
-    this.message = 'Пользователь с таким именем уже существует';
-    this.statusCode = 409;
-  }
-};
--- backend/middlewares/error.js
+++
@@ -1,7 +0,0 @@
-module.exports.errorHandler = ((err, req, res, next) => {
-  if (err.statusCode) {
-    res.status(err.statusCode).send({ message: err.message });
-  }
-  res.status(500).send({ message: 'Неизвестная ошибка)' });
-  next();
-});

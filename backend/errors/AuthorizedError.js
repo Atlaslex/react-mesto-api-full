7740ae83a933ed0aaf9863bfd8a8899f674dd976
Atlaslex <atlaslex@yandex.ru>
2022-10-28 15:35:55 +1200

    0001_05

--- backend/errors/AuthorizedError.js
+++ backend/errors/AuthorizedError.js
@@ -1,7 +1,17 @@
-module.exports = class AuthorizedError extends Error {
+// module.exports = class AuthorizedError extends Error {
+//   constructor(message) {
+//     super(message);
+//     this.message = 'Авторизуйтесь для доступа';
+//     this.statusCode = 401;
+//   }
+// };
+
+class AuthorizedError extends Error {
   constructor(message) {
     super(message);
-    this.message = 'Авторизуйтесь для доступа';
+    this.name = 'LoginError';
     this.statusCode = 401;
   }
-};
+}
+
+module.exports = AuthorizedError;
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,35 +1,54 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/user');
-const AuthorizedError = require('../errors/AuthorizedError');
+const AuthorizationError = require('../errors/AuthorizedError');
+const { NOT_REGISTERED_MESSAGE } = require('../utils/constants');
 
-const SECRET_KEY = 'very_secret';
+const { NODE_ENV, JWT_SECRET_KEY } = process.env;
 
-const throwUnauthorizedError = () => {
-  const error = new Error('Авторизуйтесь для доступа');
-  error.statusCode = 401;
-  throw error;
+module.exports = (req, res, next) => {
+  console.log(NODE_ENV);
+  const token = req.cookies.jwt;
+  let payload;
+  try {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET_KEY : 'dev-secret');
+  } catch (err) {
+    next(new AuthorizationError(NOT_REGISTERED_MESSAGE));
+  }
+  req.user = payload;
+  return next();
 };
 
-const isAuthorized = ((req, res, next) => {
-  const { authorization } = req.headers;
+// const jwt = require('jsonwebtoken');
+// const User = require('../models/user');
+// const AuthorizedError = require('../errors/AuthorizedError');
 
-  if (!authorization) {
-    throw throwUnauthorizedError();
-  }
-  try {
-    const token = () => jwt.verify(authorization.replace('Bearer ', ''), SECRET_KEY);
-    const payload = token();
-    User.findOne({ id: payload._id }).then((user) => {
-      if (!user) {
-        throwUnauthorizedError();
-      }
-    });
-    req.user = payload;
-  } catch (err) {
-    next(new AuthorizedError('Авторизуйтесь для доступа'));
-  }
+// const SECRET_KEY = 'very_secret';
 
-  next();
-});
+// const throwUnauthorizedError = () => {
+//   const error = new Error('Авторизуйтесь для доступа');
+//   error.statusCode = 401;
+//   throw error;
+// };
 
-module.exports = { isAuthorized };
+// const isAuthorized = ((req, res, next) => {
+//   const { authorization } = req.headers;
+
+//   if (!authorization) {
+//     throw throwUnauthorizedError();
+//   }
+//   try {
+//     const token = () => jwt.verify(authorization.replace('Bearer ', ''), SECRET_KEY);
+//     const payload = token();
+//     User.findOne({ id: payload._id }).then((user) => {
+//       if (!user) {
+//         throwUnauthorizedError();
+//       }
+//     });
+//     req.user = payload;
+//   } catch (err) {
+//     next(new AuthorizedError('Авторизуйтесь для доступа'));
+//   }
+
+//   next();
+// });
+
+// module.exports = { isAuthorized };

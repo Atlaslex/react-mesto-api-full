9e3c54c713b7cd4c64943035a6d3f8785b30c71b
Atlaslex <atlaslex@yandex.ru>
2022-10-28 15:45:29 +1200

    0001_06

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,42 +1,39 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const NotDataError = require('../errors/NotPassOrEmail');
-const { LinksRegExp } = require('../utils/all-reg-exp');
-
-const {
-  userNameValidator,
-  userAboutValidator,
-  userEmailValidator,
-} = require('../validators/validators');
+const bcrypt = require('bcryptjs');
+const validator = require('validator');
+const AuthorizationError = require('../errors/AuthorizedError');
+const { LOGIN_ERROR_MESSAGE } = require('../utils/constants');
+const { validationLink } = require('../middlewares/validation');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: false,
+    minlength: 2,
+    maxlength: 30,
     default: 'Жак-Ив Кусто',
-    validate: userNameValidator,
   },
   about: {
     type: String,
+    minlength: 2,
+    maxlength: 30,
     default: 'Исследователь',
-    validate: userAboutValidator,
   },
   avatar: {
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
-      validator(v) {
-        return LinksRegExp.test(v);
-      },
-      message: (props) => `${props.value} Неверный формат URL-ссылки!`,
+      validator: validationLink,
+      message: 'Ссылка не является валидной',
     },
   },
-
   email: {
     type: String,
-    unique: true,
     required: true,
-    validate: userEmailValidator,
+    unique: true,
+    validate: {
+      validator: (email) => validator.isEmail(email),
+      message: 'Значение не соответствует Email',
+    },
   },
   password: {
     type: String,
@@ -49,13 +46,80 @@ userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
-        throw new NotDataError();
+        return Promise.reject(new AuthorizationError(LOGIN_ERROR_MESSAGE));
       }
-      return Promise.all([
-        user,
-        bcrypt.compare(password, user.password),
-      ]);
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new AuthorizationError(LOGIN_ERROR_MESSAGE));
+          }
+
+          return user;
+        });
     });
 };
 
 module.exports = mongoose.model('user', userSchema);
+
+// const mongoose = require('mongoose');
+// const bcrypt = require('bcrypt');
+// const NotDataError = require('../errors/NotPassOrEmail');
+// const { LinksRegExp } = require('../utils/all-reg-exp');
+
+// const {
+//   userNameValidator,
+//   userAboutValidator,
+//   userEmailValidator,
+// } = require('../validators/validators');
+
+// const userSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: false,
+//     default: 'Жак-Ив Кусто',
+//     validate: userNameValidator,
+//   },
+//   about: {
+//     type: String,
+//     default: 'Исследователь',
+//     validate: userAboutValidator,
+//   },
+//   avatar: {
+//     type: String,
+//     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+//     validate: {
+//       validator(v) {
+//         return LinksRegExp.test(v);
+//       },
+//       message: (props) => `${props.value} Неверный формат URL-ссылки!`,
+//     },
+//   },
+
+//   email: {
+//     type: String,
+//     unique: true,
+//     required: true,
+//     validate: userEmailValidator,
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//     select: false,
+//   },
+// });
+
+// userSchema.statics.findUserByCredentials = function (email, password) {
+//   return this.findOne({ email }).select('+password')
+//     .then((user) => {
+//       if (!user) {
+//         throw new NotDataError();
+//       }
+//       return Promise.all([
+//         user,
+//         bcrypt.compare(password, user.password),
+//       ]);
+//     });
+// };
+
+// module.exports = mongoose.model('user', userSchema);

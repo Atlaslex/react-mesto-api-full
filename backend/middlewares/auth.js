6f3b77b1cf026d3eb0cc612e2ff1e70e6585a84c
Atlaslex <atlaslex@yandex.ru>
2022-10-31 08:56:34 +1200

    01_011

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,14 +2,14 @@ const jwt = require('jsonwebtoken');
 const AuthorizationError = require('../errors/AuthorizationError');
 const { NOT_REGISTERED_MESSAGE } = require('../utils/constants');
 
-const { NODE_ENV, JWT_SECRET_KEY } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   console.log(NODE_ENV);
   const token = req.cookies.jwt;
   let payload;
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET_KEY : 'dev-secret');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     next(new AuthorizationError(NOT_REGISTERED_MESSAGE));
   }
